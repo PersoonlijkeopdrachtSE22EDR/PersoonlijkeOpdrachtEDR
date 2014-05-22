b448f2d50b84b2bc105ab7c687857bc02406e123
EricDeRegter <ericderegter@gmail.com>
2014-05-22 20:22:47 +0200

    Gewerkt aan account en account database properties

--- Webshop2/Webshop2/Model/Account.cs
+++ Webshop2/Webshop2/Model/Account.cs
@@ -7,5 +7,46 @@ namespace Webshop.Model
 {
     public partial class Account
     {
+        public string Gebruikersnaam
+        {
+            get;
+            set;
+        }
+
+        private string wachtwoord;
+
+        public string Naam
+        {
+            get;
+            set;
+        }
+
+        public string Adres
+        {
+            get;
+            set;
+        }
+
+        public string Telefoonnummer
+        {
+            get;
+            set;
+        }
+
+        public List<Bestelling> Bestellingen
+        {
+            get
+            {
+                return Bestelling.getBestellingen(this.Gebruikersnaam); //parameter nog invullen
+            }
+        }
+
+        public Winkelwagen Winkelwagen
+        {
+            get;
+            set;
+        }
+
+        public Account (string gebruikersnaam, string wachtwoord, string naam, string adres, string telefoonnummer)
     }
 }
--- Webshop2/Webshop2/Model/Account.database.cs
+++ Webshop2/Webshop2/Model/Account.database.cs
@@ -13,10 +13,17 @@ namespace Webshop.Model
         public static List<Account> GetAccounts()
         {
             List<Account> Accounts = new List<Account>();
-            DataTable dt = Database.getData("SELECT ");//NOG VERDER MAKEN
+            DataTable dt = Database.getData("SELECT EMAILADRES as email, WACHTWOORD, NAAM, ADRES, TELEFOONNUMMER, WOONPLAATS FROM ACCOUNTS");//NOG VERDER MAKEN
             foreach(DataRow row in dt.Rows)
             {
+                string email = row["email"].ToString();
+                string wachtwoord = row["WACHTWOORD"].ToString();
+                string naam = row["NAAM"].ToString();
+                string adres = row["ADRES"].ToString();
+                string telefoonnummer = row["TELEFOONNUMMER"].ToString();
+                string woonplaats = row["WOONPLAATS"].ToString();
 
+                Account account = new Account
             }
             return Accounts;
         }
