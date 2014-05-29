4d756bda3a9dde385f43a0d0a0d8af51018d91ff
EricDeRegter <ericderegter@gmail.com>
2014-05-29 15:14:10 +0200

    email werkt nu, opmaak en via gmail

--- Webshop2/Webshop2/Model/Bestelling.cs
+++ Webshop2/Webshop2/Model/Bestelling.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Net.Mail;
+using System.Net;
 
 namespace Webshop2
 {
@@ -33,21 +34,25 @@ namespace Webshop2
             this.Datum = datum;
         }
 
-        public void PlaatsBestelling()
+        public void PlaatsBestelling(string email, Product product)
         {
-            SmtpClient smtpClient = new SmtpClient("mail.MyWebsiteDomainName.com", 25);
-
-            smtpClient.Credentials = new System.Net.NetworkCredential("info@MyWebsiteDomainName.com", "myIDPassword");
-            smtpClient.UseDefaultCredentials = true;
-            smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
-            smtpClient.EnableSsl = true;
+            SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+            SmtpServer.Port = 587;
+            SmtpServer.UseDefaultCredentials = false;
+            SmtpServer.Credentials = new System.Net.NetworkCredential("webshopEDR@gmail.com", "webshop1");
+            SmtpServer.EnableSsl = true;
             MailMessage mail = new MailMessage();
+            mail.Subject = "Uw bestelling bij EDR Webshop";
+            mail.IsBodyHtml = true;
+            string body = "<h3>Bedankt voor het plaatsen van een bestelling bij EDR Webshop.</h3><br /><p>Productdetails</p><p><ul><li>Artikelnummer: " + product.Artikelnummer.ToString() + "</li><li>Productnaam: " + product.Productnaam + "</li><li>Prijs: €" + product.Prijs.ToString() + "</li><li>Beschrijving: " + product.Beschrijving + "</li></ul></p> ";
+            mail.Body = body;
+            
 
             //Setting From , To and CC
-            mail.From = new MailAddress("info@MyWebsiteDomainName", "MyWeb Site");
-            mail.To.Add(new MailAddress("ericderegter@gmail.com"));
+            mail.From = new MailAddress("webshopEDR@gmail.com", "Webshop EDR");
+            mail.To.Add(new MailAddress(email));
 
-            smtpClient.Send(mail);
+            SmtpServer.Send(mail);
         }
     }
 }
